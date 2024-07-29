6f51b4854e150321ce6513dfd4dc8a38f8484417
Peter Duits <peterduits@live.nl>
2024-07-29 20:17:40 +0200

    Install Google Adsense

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,21 +2,12 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
-    modules: ['@nuxtjs/tailwindcss', '@primevue/nuxt-module'],
+    modules: ['@nuxtjs/tailwindcss', '@primevue/nuxt-module', '@nuxtjs/google-adsense'],
     css: ['@/assets/css/main.scss'],
     primevue: {
         importTheme: { from: '@/themes/mytheme.js' },
     },
-    app: {
-        head: {
-            script: [
-                {
-                    hid: 'adsense',
-                    src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-                    async: true,
-                    'data-ad-client': '8907205065732865',
-                },
-            ],
-        },
+    googleAdsense: {
+        id: 'ca-pub-8907205065732865',
     },
 });
