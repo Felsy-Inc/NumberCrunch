8ed53c9102235a65c749049a438de835d33b4f86
Peter Duits <peterduits@live.nl>
2024-07-29 20:41:21 +0200

    Fix styling bug on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
-    <IntroductionHero />
+    <main class="home-page">
+        <IntroductionHero />
+    </main>
 </template>
 
 <script setup>
 import IntroductionHero from '~/components/IntroductionHero.vue';
 </script>
+
+<style lang="scss">
+.home-page {
+    @apply relative;
+}
+</style>
