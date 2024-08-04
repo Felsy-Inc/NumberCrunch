8c9d337249df9bd0e9b322f0dc53e129e4402199
Peter Duits <peterduits@live.nl>
2024-08-04 15:55:52 +0200

    Fix header

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="app-header">
         <div class="app-header-container">
-            <Menubar :model="items" class="app-header-nav">
+            <MegaMenu :model="items" class="app-header-nav">
                 <template #start>
                     <RouterLink to="/">
                         <h1 class="app-header__title">NumberCrunch.io</h1>
@@ -29,17 +29,15 @@
                     <i class="pi pi-bars text-xl" />
                 </template>
                 <template #end>
-                    <DecimalSeparatorToggle />
                     <DarkModeToggle />
                 </template>
-            </Menubar>
+            </MegaMenu>
         </div>
     </header>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import DecimalSeparatorToggle from '../base/DecimalSeparatorToggle.vue';
 import DarkModeToggle from '../base/DarkModeToggle.vue';
 
 const items = ref([
@@ -47,49 +45,123 @@ const items = ref([
         label: 'Calculators',
         icon: 'pi pi-calculator',
         items: [
-            // {
-            //     label: 'Basic',
-            //     icon: 'pi pi-dollar',
-            //     route: '/calculator/basic',
-            // },
-            // {
-            //     label: 'BMI',
-            //     icon: 'pi pi-chart-bar',
-            //     route: '/calculator/bmi',
-            // },
-            // {
-            //     label: 'Mortgage',
-            //     icon: 'pi pi-home',
-            //     route: '/calculator/mortgage',
-            // },
-            // {
-            //     label: 'Currency',
-            //     icon: 'pi pi-money-bill',
-            //     route: '/currency-converter',
-            // },
-            {
-                label: 'Percentage',
-                icon: 'pi pi-percentage',
-                route: '/calculator/percentage',
-            },
+            [
+                {
+                    label: 'Mathematical',
+                    items: [
+                        {
+                            label: 'Percentage',
+                            icon: 'pi pi-percentage',
+                            route: '/calculator/percentage',
+                        },
+                    ],
+                },
+            ],
+            [
+                {
+                    label: 'Health',
+                    items: [
+                        {
+                            label: 'BMI',
+                            icon: 'pi pi-chart-bar',
+                            route: '/calculator/bmi',
+                        },
+                    ],
+                },
+            ],
+            [
+                {
+                    label: 'Financial',
+                    items: [
+                        {
+                            label: 'Mortgage',
+                            icon: 'pi pi-home',
+                            route: '/calculator/mortgage',
+                        },
+                    ],
+                },
+            ],
         ],
     },
-    {
-        label: 'Randomizers',
-        icon: 'pi pi-sync',
-        items: [
-            {
-                label: 'Random Number',
-                icon: 'pi pi-sort-numeric-down',
-                route: '/randomizer/number',
-            },
-            // {
-            //     label: 'Random Letter',
-            //     icon: 'pi pi-sort-alpha-down',
-            //     route: '/randomizer/letter',
-            // },
-        ],
-    },
+    // {
+    //     label: 'Randomizers',
+    //     icon: 'pi pi-sync',
+    //     items: [
+    //         [
+    //             {
+    //                 label: 'Basic',
+    //                 items: [
+    //                     {
+    //                         label: 'Addition',
+    //                         icon: 'pi pi-plus',
+    //                         route: '/calculator/basic/addition',
+    //                     },
+    //                     {
+    //                         label: 'Subtraction',
+    //                         icon: 'pi pi-minus',
+    //                         route: '/calculator/basic/subtraction',
+    //                     },
+    //                     {
+    //                         label: 'Multiplication',
+    //                         icon: 'pi pi-times',
+    //                         route: '/calculator/basic/multiplication',
+    //                     },
+    //                     {
+    //                         label: 'Division',
+    //                         icon: 'pi pi-divide',
+    //                         route: '/calculator/basic/division',
+    //                     },
+    //                 ],
+    //             },
+    //         ],
+    //         [
+    //             {
+    //                 label: 'Mathematical',
+    //                 items: [
+    //                     {
+    //                         label: 'Percentage',
+    //                         icon: 'pi pi-percentage',
+    //                         route: '/calculator/percentage',
+    //                     },
+    //                     {
+    //                         label: 'Square Root',
+    //                         icon: 'pi pi-square-root',
+    //                         route: '/calculator/square-root',
+    //                     },
+    //                     {
+    //                         label: 'Cube Root',
+    //                         icon: 'pi pi-cube',
+    //                         route: '/calculator/cube-root',
+    //                     },
+    //                 ],
+    //             },
+    //         ],
+    //         [
+    //             {
+    //                 label: 'Health',
+    //                 items: [
+    //                     {
+    //                         label: 'BMI',
+    //                         icon: 'pi pi-chart-bar',
+    //                         route: '/calculator/bmi',
+    //                     },
+    //                 ],
+    //             },
+    //         ],
+    //         [
+    //             {
+    //                 label: 'Utility',
+    //                 items: [
+    //                     {
+    //                         label: 'Mortgage',
+    //                         icon: 'pi pi-home',
+    //                         route: '/calculator/mortgage',
+    //                     },
+    //                 ],
+    //             },
+    //         ],
+    //     ],
+    // },
 ]);
 </script>
 
@@ -109,12 +181,13 @@ const items = ref([
         @apply text-2xl sm:text-3xl font-bold text-primary-500 dark:text-primary-400;
     }
 
-    .p-menubar {
+    .p-megamenu-horizontal .p-megamenu,
+    .p-megamenu {
         @apply border-none bg-transparent;
 
         &-start {
             @apply w-full;
-            @media (min-width: 1024px) {
+            @media (min-width: 961px) {
                 @apply w-auto;
             }
         }
