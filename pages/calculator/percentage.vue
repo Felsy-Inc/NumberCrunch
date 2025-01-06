501aa3ce8c1a50917e10273f83237560e64d9bee
Peter Duits <peterduits@live.nl>
2025-01-06 08:35:36 +0100

    Extract page styling to global styling file

--- pages/calculator/percentage.vue
+++ pages/calculator/percentage.vue
@@ -1,6 +1,6 @@
 <template>
-    <main class="percentage-page">
-        <!-- <h1 class="percentage-page__title">Percentage Calculators</h1> -->
+    <main class="calculator-page">
+        <!-- <h1 class="calculator-page__title">Percentage Calculators</h1> -->
         <PercentageOfNumber />
         <!-- <HorizontalLargeAd /> -->
     </main>
@@ -10,14 +10,3 @@
 // import HorizontalLargeAd from '~/components/ads/HorizontalLargeAd.vue';
 import PercentageOfNumber from '~/components/calculators/percentage/PercentageOfNumber.vue';
 </script>
-
-<style lang="scss">
-.percentage-page {
-    @apply relative flex-1 my-8 lg:my-24;
-
-    &__title {
-        @apply py-8 lg:py-24 text-2xl sm:text-6xl font-bold text-center mx-auto w-fit bg-gradient-to-r 
-        from-primary-700 via-primary-400 to-primary-700 dark:from-primary-600 dark:via-primary-300 dark:to-primary-600 text-transparent bg-clip-text;
-    }
-}
-</style>
