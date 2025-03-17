24c481376114eff533a551188bdbbf63cafd5dd6
Peter Duits <peterduits@live.nl>
2025-03-17 12:49:22 +0100

    Fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
+    telemetry: false,
     modules: [
         '@nuxtjs/tailwindcss',
         '@primevue/nuxt-module',
