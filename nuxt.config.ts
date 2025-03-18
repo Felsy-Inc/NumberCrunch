216cd64f57b5c8a4661ccd289ff139a7df25a488
Peter Duits <peterduits@live.nl>
2025-03-18 09:01:38 +0100

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,7 +67,36 @@ export default defineNuxtConfig({
                 { name: 'google-adsense-account', content: 'ca-pub-8907205065732865' },
             ],
             link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '16x16',
+                    href: '/favicon/favicon-16x16.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '32x32',
+                    href: '/favicon/favicon-32x32.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '192x192',
+                    href: '/favicon/android-chrome-192x192.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '512x512',
+                    href: '/favicon/android-chrome-512x512.png',
+                },
+                {
+                    rel: 'apple-touch-icon',
+                    sizes: '180x180',
+                    href: '/favicon/apple-touch-icon.png',
+                },
                 { rel: 'canonical', href: 'https://numbercrunch.io' },
             ],
             script: [
